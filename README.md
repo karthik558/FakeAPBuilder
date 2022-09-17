61c7d0118473014c8b4f2bb5d15622c3774c1307
KARTHIK LAL <karthik.lal558@gmail.com>
2022-09-17 14:58:11 +0530

    script/mitm-attack: Update whole codebase including lst and readme to V2
    Signed-off-by: KARTHIK LAL <karthik.lal558@gmail.com>

--- README.md
+++ README.md
@@ -1,38 +1,71 @@
+![HEADER_IMAGE](src/mitm.png)
+
 ## <b> ABOUT THIS PROJECT </b>
 
-* This project is a simple bash script that will help you to create lot's of FAKE_AP (MITM attack).
-* This script will create a lot of fake AP's and will help you to test your network and network adapter. (pentest own network)
-* Don't use this script for illegal purposes. I am not responsible for any damage that may occur.
-* This script is not 100% accurate.
+This MITM attack is based on the MDK3 tool. MDK3 is a tool that can be used to create fake APs and MITM attacks. This script is just a wrapper around the MDK3 tool. It will check if the MDK3 tool is installed or not. If not, it will install it. Then it will check if the packet injection is working or not on the interface 0/1. If not, it will abort the script. Then it will ask the user to select the correct interface to create the fake AP. It will create the fake AP on the selected interface and will start the MITM attack. The fake AP will be created using the file.lst file. The file.lst file contains the SSID and the password of the fake AP. The user can change the SSID and the password of the fake AP by editing the file.lst file.
 
-## <b> HOW TO USE THIS PROJECT </b>
+## <b> USAGE OF THIS SCRIPT (HOW TO USE) </b>
 
-* ` git clone https://github.com/karthik558/CREATE-MITM-AP.git `
-* ` cd CREATE-MITM-AP `
-* ` chmod +x start.sh `
-* ` ./start.sh `
+### This script will create a lot of fake AP's and will help you to test your network and network adapter. (pentest own network)
 
-## <b> INFORMATION ABOUT THIS PROJECT </b>
+├── 1. Download the script from the github repository. <br>
+├── 2. Open the terminal and navigate to the directory where the script is downloaded.<br>
+├── 3. Run the script using the command "sudo ./start.sh" (without the quotes).<br>
+├── 4. The script will ask you to select the correct interface to test the packet injection.<br>
+├── 5. Select the correct interface to test the packet injection.<br>
+├── 6. The script will ask you to select the correct interface to create the fake AP.<br>
+├── 7. Select the correct interface to create the fake AP. (The fake AP will be created using the file.lst file)<br>
+├── 8. The script will create the fake AP and will start the MITM attack.<br>
+├── 9. The fake AP will be created using the SSID and the password of the file.lst file.<br>
+└── 10. The user can change the SSID and the password of the fake AP by editing the file.lst file.<br>
 
-* Only run this script on root. (sudo) If you are not root, then you will not be able to run this script.
-* Only run this script on Linux.
-* Depending on your Linux distribution, you may need to install some packages. (recommended to install `aircrack-ng`,`airmon-ng`,`mdk3`.`airodump-ng`,`aireplay-ng`,`xterm` or else you will not be able to run this script.)
-* This script need external_wireless_adapter. (or else it will not work).
-* If you are using TL-WN722N, then you need to install `tl-wn722n-driver` package. (recommended to install `tl-wn722n-driver`.) use my github link to download it. ( https://github.com/karthik558/setup_kali_env/tree/main/TP-LINK )
+## <b> DISCLAIMER </b>
+
+├── 1. Run this script only with <b> sudo privileges </b>. (sudo ./start.sh) (without the quotes) <br>
+├── 2. Depending on your Linux distribution, you may need to install some packages. (recommended to install `aircrack-ng`,`airmon-ng`,`mdk3`.`airodump-ng`,`aireplay-ng`,`xterm` or else you will not be able to run this script.) <br>
+├── 3. This script need external_wireless_adapter. (or else it will not work). <br>
+├── 4. If you are using TL-WN722N, then you need to install `TL-WN722N-driver` package. (recommended to install `TL-WN722N-driver`.) use my github link to download it. ( https://github.com/karthik558/setup_kali_env/tree/main/TP-LINK ) <br>
+└── 5. This script is for educational purposes only. The author is not responsible for any misuse or damage caused by this script. Use it at your own risk. <br>
 
 ## <b> THANKS TO </b>
 
-* aircrack-ng (https://www.aircrack-ng.org/)
-* airmon-ng (https://www.aircrack-ng.org/)
-* mdk3 (https://www.mdk3.com/)
-* airodump-ng (https://www.aircrack-ng.org/)
-* aireplay-ng (https://www.aircrack-ng.org/)
-* kali linux (https://www.kali.org/)
+├── Aircrack-ng (https://www.aircrack-ng.org/) <br>
+├── Airmon-ng (https://www.aircrack-ng.org/) <br>
+├── MDK3 (https://www.mdk3.com/) <br>
+├── Airodump-ng (https://www.aircrack-ng.org/) <br>
+├── Aireplay-ng (https://www.aircrack-ng.org/) <br>
+└── Kali Linux (https://www.kali.org/) <br>
 
-## <b> CONTRIBUTION </b>
+## <b> CONTRIBUTE TO THIS PROJECT </b>
 
-* If you want to contribute to this project, then follow github-basic-contribution rules. (inlude signoff and commit message) (https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/writing-a-pull-request)
-* GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
-* MIT License (https://opensource.org/licenses/MIT)
-* Open Source Initiative (OSI) (https://opensource.org/licenses/OSL-3.0)
-* PROJECT LICENSE ( @karthik558/FAKE-MITM-AP/LICENSE )
+### If you want to contribute to this project, then you can fork this project and make changes to it. Then you can make a pull request. I will review your changes and if it is good, then I will merge it to the main branch.
+
+ ├── 1. Fork the project.  <br>
+ ├── 2. Clone the project to your local machine.  <br>
+ ├── 3. Create a new branch.  <br>
+ ├── 4. Make the changes.  <br>
+ ├── 5. Commit the changes. (include a brief description of the changes)  <br>
+ ├── 6. Push the changes to the remote repository.  <br>
+ └── 7. Create a pull request. <br>
+
+## <b> AUTHOR OF THIS PROJECT </b>
+
+Author: KARTHIK LAL (karthik558) <br>
+Author Website: https://karthiklal.live <br>
+
+ ## <b> CONTACT AUTHOR </b>
+
+├── 1. Email: karthik.lal558@gmail.com | karthiklal@duck.com <br>
+├── 2. Website: https://karthiklal.live#contact <br>
+├── 3. Twitter: https://twitter.com/_karthik558 <br>
+├── 4. Instagram: https://instagram.com/_karthiklal <br>
+├── 5. Facebook: https://facebook.com/karthik5588 <br>
+├── 6. LinkedIn: https://linkedin.com/in/karthik558 <br>
+└── 7. GitHub: https://github.com/karthik558 <br>
+
+## <b> LICENSE </b>
+
+This script is licensed under the GNU General Public License V3.0<br>
+*  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+*  MITM ATTACK is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
--- start.sh
+++ start.sh
@@ -1,79 +1,144 @@
 #!/bin/bash
 
-# Abort if its not running on root
-   echo "Checking if you are running this script on su mode or not"
+# Color Palette for Terminal
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+VIOLET='\033[0;35m'
+CYAN='\033[0;36m'
+BLACK='\033[0;30m'
+NC='\033[0m' # No Color
+
+# Print the banner
+#  ██████╗██████╗ ███████╗ █████╗ ████████╗███████╗    ███╗   ███╗██╗████████╗███╗   ███╗       █████╗ ██████╗
+# ██╔════╝██╔══██╗██╔════╝██╔══██╗╚══██╔══╝██╔════╝    ████╗ ████║██║╚══██╔══╝████╗ ████║      ██╔══██╗██╔══██╗
+# ██║     ██████╔╝█████╗  ███████║   ██║   █████╗      ██╔████╔██║██║   ██║   ██╔████╔██║█████╗███████║██████╔╝
+# ██║     ██╔══██╗██╔══╝  ██╔══██║   ██║   ██╔══╝      ██║╚██╔╝██║██║   ██║   ██║╚██╔╝██║╚════╝██╔══██║██╔═══╝
+# ╚██████╗██║  ██║███████╗██║  ██║   ██║   ███████╗    ██║ ╚═╝ ██║██║   ██║   ██║ ╚═╝ ██║      ██║  ██║██║
+#  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝    ╚═╝     ╚═╝╚═╝   ╚═╝   ╚═╝     ╚═╝      ╚═╝  ╚═╝╚═╝
+
+# Abort if its not running on root user
+printf "${YELLOW}Checking if you are running this script on su mode or not ${NC}"
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root"
+   printf "${RED}This script must be run as root ${NC}"
    sleep 1
    exit
-else 
-   sleep 2
+else
+   sleep 1
 fi
 
-# Abort if its not running on linux
-   echo "Checking if you are running this script on linux or not"
+# Abort if its not running on linux machine
+printf "${YELLOW}Checking if you are running this script on linux or not ${NC}"
 if [[ $(uname -s) != "Linux" ]]; then
-   echo "This script must be run on linux"
+   printf "${RED}This script must be run on linux ${NC}"
    sleep 1
    exit
-else 
-   sleep 2
+else
+   sleep 1
 fi
 
-# Install MDK3 to run this tool
-
-sudo apt install mdk3
+# Install MDK3 if not installed already
+printf "${YELLOW}Checking if MDK3 is installed or not ${NC}"
+if ! command -v mdk3 &>/dev/null; then
+   printf "${RED}MDK3 is not installed ${NC}"
+   printf "${YELLOW}Installing MDK3 ${NC}"
+   apt-get install mdk3 -y
+   sleep 1
+else
+   printf "${GREEN}MDK3 is already installed ${NC}"
+   sleep 1
+fi
 
 # Check if packet injection is working or not on interface 0/1
-echo "Select the correct interface to test packet-injection ! (0/1)"
+printf "${YELLOW}Select the correct interface to test packet-injection ! (0/1) ${NC}"
 read -r answer
 if [[ $answer == 0 ]]; then
-   echo "Injecting packets on interface 0"
-   ifconfig wlan0 down;
-   iwconfig wlan0 mode monitor;
-   ifconfig wlan0 up;
-   iwconfig wlan0;
-   echo "Injecting packets on interface 0"
+   printf "${RED}Starting the injection test on WLAN0... ${NC}"
+   ifconfig wlan0 down
+   iwconfig wlan0 mode monitor
+   ifconfig wlan0 up
+   iwconfig wlan0
+   printf "${GREEN}Injecting packets on interface 0 ${NC}"
    aireplay-ng --test wlan0
-   echo "Packet injection test done on interface 0 and its working fine"
-else 
-   echo "Injecting packets on interface 1"
-   ifconfig wlan1 down;
-   iwconfig wlan1 mode monitor;
-   ifconfig wlan1 up;
-   iwconfig wlan1;
-   echo "Injecting packets on interface 1"
+   printf "${GREEN}Packet injection test done on interface 0 and its working fine ${NC}"
+else
+   printf "${RED}Starting the injection test on WLAN1... ${NC}"
+   ifconfig wlan1 down
+   iwconfig wlan1 mode monitor
+   ifconfig wlan1 up
+   iwconfig wlan1
+   printf "${GREEN}Injecting packets on interface 1 ${NC}"
    aireplay-ng --test wlan1
-   echo "Packet injection test done on interface 1 and its working fine"
+   printf "${GREEN}Packet injection test done on interface 1 and its working fine ${NC}"
 fi
 
 # Run the MDK3 tool to create MITM AP using our created file.lst file (select the correct interface/wlan0/wlan1)
-echo "Select the correct interface to create FAKE_AP ! (0/1)"
+printf "${YELLOW}Select the correct interface to create FAKE_AP ! (0/1) ${NC}"
 read -r answer
 if [[ $answer == 0 ]]; then
-   echo "Creating fake AP on interface 0"
+   printf "${RED}Creating fake AP on interface 0 ${NC}"
    mdk3 wlan0 b -c 1 -f exploit.lst
-   echo "Fake AP created on interface 0"
+   printf "${GREEN}Fake AP created on interface 0 ${NC}"
 else
-    echo "Creating fake AP on interface 1"
-    mdk3 wlan1 b -c 1 -f exploit.lst
-    echo "Fake AP created on interface 1"
+   printf "${RED}Creating fake AP on interface 1 ${NC}"
+   mdk3 wlan1 b -c 1 -f exploit.lst
+   printf "${GREEN}Fake AP created on interface 1 ${NC}"
 fi
 
 ##
-## LICENSE: GPL-3.0-or-later
-## Author: KARTHIK LAL
-## Copyright (C) 2022 KARTHIK LAL
+## Brief Explanation of the above code.
+## Author Information
+## License Information
+## Usage of the script (how to use it)
+## How to contribute to the project
+## How to contact the author
+##
+
+##1
+## This MITM attack is based on the MDK3 tool. MDK3 is a tool that can be used to create fake APs and MITM attacks. This script is just a wrapper around the MDK3 tool. It will check if the MDK3 tool is installed or not. If not, it will install it. Then it will check if the packet injection is working or not on the interface 0/1. If not, it will abort the script. Then it will ask the user to select the correct interface to create the fake AP. It will create the fake AP on the selected interface and will start the MITM attack. The fake AP will be created using the file.lst file. The file.lst file contains the SSID and the password of the fake AP. The user can change the SSID and the password of the fake AP by editing the file.lst file.
+
+##2
+## Author: KARTHIK LAL (karthik558)
+## Author Website: https://karthiklal.live
+
+##3
+## This script is licensed under the GNU General Public License v3.0
 ## This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 ## MITM ATTACK is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 ## You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
-##
-# PROJECT LICENSE ( @karthik558/FAKE-MITM-AP/LICENSE )
-##
-## HOW TO USE THIS SCRIPT
-## 1. Clone the script from github
-## 2. Run the script with ./start.sh
-## 3. You will be asked to select the correct interface to create fake AP (0/1) and to select the correct interface to test packet-injection (0/1)
-## 4. You can edit the exploit.sh file and edit those variables to your liking
-## 5. Thats it!
-##
+
+##4
+## Usage of the script (how to use it)
+# ├── 1. Download the script from the github repository.
+# ├── 2. Open the terminal and navigate to the directory where the script is downloaded.
+# ├── 3. Run the script using the command "sudo ./start.sh" (without the quotes).
+# ├── 4. The script will ask you to select the correct interface to test the packet injection.
+# ├── 5. Select the correct interface to test the packet injection.
+# ├── 6. The script will ask you to select the correct interface to create the fake AP.
+# ├── 7. Select the correct interface to create the fake AP. (The fake AP will be created using the file.lst file)
+# ├── 8. The script will create the fake AP and will start the MITM attack.
+# ├── 9. The fake AP will be created using the SSID and the password of the file.lst file.
+# └── 10. The user can change the SSID and the password of the fake AP by editing the file.lst file.
+
+##5
+## How to contribute to the project
+# ├── 1. Fork the project.
+# ├── 2. Clone the project to your local machine.
+# ├── 3. Create a new branch.
+# ├── 4. Make the changes.
+# ├── 5. Commit the changes. (include a brief description of the changes)
+# ├── 6. Push the changes to the remote repository.
+# └── 7. Create a pull request.
+
+##6
+## How to contact the author
+# ├── 1. Email: karthik.lal558@gmail.com | karthiklal@duck.com
+# ├── 2. Website: https://karthiklal.live#contact
+# ├── 3. Twitter: https://twitter.com/_karthik558
+# ├── 4. Instagram: https://instagram.com/_karthiklal
+# ├── 5. Facebook: https://facebook.com/karthik5588
+# ├── 6. LinkedIn: https://linkedin.com/in/karthik558
+# └── 7. GitHub: https://github.com/karthik558
+
+
